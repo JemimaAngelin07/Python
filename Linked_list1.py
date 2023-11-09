e4914b1146e5ec14d7854578d04c8e2476939518
JemimaAngelin07 <150262131+JemimaAngelin07@users.noreply.github.com>
2023-11-09 19:10:52 +0530

    Add files via upload

---
+++ Linked_list1.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self,data=None):
+        self.data=data
+        self.next=None
+class singly_linked_list:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        self.count=0
+def append_item(self,data):
+    node=Node(data)
+    if self.head:
+        self.head.next=node
+        self.head=node
+    else:
+        self.head=node
+        self.tail=node
+    self.count += 1
+def iterate_item(self):
+    current_item=self.tail
+    while current_item:
+        val=current_item.data
+        current_item=current_item.next
+        yield val                            
+
+items=singly_linked_list()
+items.append_item("PHP")
+items.append_item("Python")
+items.append_item("Java")
+items.append_item("C#")
+items.append_item("C++")
+for val in items.iterate_item():
+    print(val)
+print("\nHead Node:",items.head.data)
+print("Tail Node:",items.tail.data)    
+
+
+
+
+
